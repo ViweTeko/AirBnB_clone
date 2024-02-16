14b283557b1c445610a0767f0c44b61a4260d2d7
ViweTeko <iamviwe.teko@gmail.com>
2024-02-16 15:07:15 +0200

    Fixed import FileStorage

--- models/__init__.py
+++ models/__init__.py
@@ -2,7 +2,7 @@
 
 """This module initializes the package"""
 
-from models.engine.file_storage import FilesStorage
+from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
 storage.reload()
